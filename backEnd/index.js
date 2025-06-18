b1cb15fc48ab91eb836e6217e32eeee5db684b89
Yug sudani <yugsudani88@gmail.com>
2025-06-18 18:36:08 +0530

    Update index.js

--- backEnd/index.js
+++ backEnd/index.js
@@ -17,8 +17,7 @@ const { isLogin } = require("./middelware/isLogin");
 const { isAdmin } = require("./middelware/isAdmin");
 
 //connection
-connectMDB("mongodb+srv://yugsudani88:Vl7CXGJuTtYisJp4@clusterecartdb.fwuvdoa.mongodb.net/?retryWrites=true&w=majority&appName=ClusterEcartDB&tls=true&tlsAllowInvalidCertificates=true");
-
+connectMDB("mongodb+srv://yugsudani88:Vl7CXGJuTtYisJp4@clusterecartdb.fwuvdoa.mongodb.net/?retryWrites=true&w=majority&appName=ClusterEcartDB");
 
 // Enabling frontend backend communications
 app.use(cors()); // allows all origins
